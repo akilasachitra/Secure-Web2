9f193a8eee8a6e184f3f921549c2f85ec6886eef
akilasachitra <akilasachitra@gmail.com>
2025-03-09 20:59:56 +0530

    demo commit

--- src/main/java/com/akimax/demo/DemoApplication.java
+++ src/main/java/com/akimax/demo/DemoApplication.java
@@ -2,9 +2,6 @@ package com.akimax.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
-import java.net.URI;
 
 @SpringBootApplication
 public class DemoApplication {
